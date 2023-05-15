39df0530c2b5b102ea75412e7a49923a5559ba9c
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-05-15 20:20:25 +0300

    feat: add state to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [state, setState] = useState({ value: 1, count: 10 });
+
+  const updateState = (key) => {
+    setState(() => {
+      const newState = { ...state };
+      newState[key] += 1;
+      return newState;
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Value: {state.value}</div>
+      <div>Count: {state.count}</div>
+      <button onClick={() => updateState("value")}>Add value</button>
+      <button onClick={() => updateState("count")}>Add count</button>
     </div>
   );
 }
